fd0ae7621dc78662c7a07089fb707da1d287653d
LuisGS <luis.ild@gmail.com>
2019-09-25 00:00:24 +0200

    create a copy of our json file

--- expensesinput.py
+++ expensesinput.py
@@ -1,29 +1,30 @@
 import sys
 import PySimpleGUI as sg
+import expenseJSONFile
 
 # Very basic window.  Return values as a list
 username = "Default"
 filename = "/tmp/deleteme.txt"
 
-categories = [[sg.Radio('Loging', "CAT", default=True), sg.Radio('Transport', "CAT"), sg.Radio('Entertainemt', "CAT"), sg.Radio('Salary', "CAT")]]
+categories = [[sg.Radio('Loging', "CAT", default=True, key="cat0"), sg.Radio('Transport', "CAT", key="cat1"), sg.Radio('Entertainemt', "CAT", key="cat2"), sg.Radio('Salary', "CAT", key="cat3")]]
 #
 # First tab layaout
 #
 
 tab1_layout =  [
-          [sg.Text('Expense Name', size=(15, 1)), sg.InputText('Expense Name')],
-          [sg.Text('Quantity', size=(15, 1)), sg.InputText('$?')],
-          [sg.Text('Frequency', size=(15, 1)), sg.Radio('Monthly', "FREQ", default=True), sg.Radio('Yearly', "FREQ")],
+          [sg.Text('Expense Name', size=(15, 1)), sg.InputText('Expense Name', key='expenseName')],
+          [sg.Text('Quantity', size=(15, 1)), sg.InputText('$?', key='qty')],
+          [sg.Text('Frequency', size=(15, 1)), sg.Radio('Monthly', "FREQ", key="freq0", default=True), sg.Radio('Yearly', "FREQ", key="freq1")],
           [sg.Frame("Categories", [[sg.Column(categories)]])],
-          [sg.Text('Date', size=(15, 1)), sg.InputText('../../..')],
-          [sg.Text('Income/Outcome', size=(15, 1)), sg.Radio('Outcome', "INOUT", default=True), sg.Radio('Income', "INOUT")],
+          [sg.Text('Date', size=(15, 1)), sg.InputText('../../..', key='date')],
+          [sg.Text('Income/Outcome', size=(15, 1)), sg.Radio('Outcome', "INOUT", default=True, key='in'), sg.Radio('Income', "INOUT", key='out')],
           [sg.Submit(key='submitTab1'), sg.Cancel(key='Cancel')]
          ]
 
 #
 # Second tab layaout
 #
-tab2_layout = [[sg.T('This is inside tab 2')], [sg.In(key='in')],
+tab2_layout = [[sg.T('This is inside tab 2')], [sg.In(key='in1')],
           [sg.Submit(key='submitTab2'), sg.Cancel(key='Cancel')]]
 
 
@@ -42,6 +43,7 @@ def main(argv):
     username = argv[0]
     filename = argv[3]
 
+    data=expenseJSONFile.readJSON(filename)
 
     window = sg.Window('Hello {}!! Please, type in all your expenses'.format(username)).Layout(layout)
 
@@ -50,6 +52,9 @@ def main(argv):
         print(button)
         if button == "submitTab1":
             sg.popup("Submit layaout 1")
+            newExpense = {}
+            print(values)
+            #expenseJSONFile.writeExpense(filename, data, expense):
         elif button == "submitTab2":
             sg.popup("Submit layaout 2")
         elif button == "submitTab3":
