767a6fe99c40b09ac867c22f72ed0d5c7cb3488c
LuisGS <luis.ild@gmail.com>
2019-09-25 00:00:05 +0200

    adding new function. write expense

--- expenseJSONFile.py
+++ expenseJSONFile.py
@@ -1,15 +1,30 @@
 import json
 
+#
+# Library for reading our JSON file
+#
 
-
+# readJSON
+# Open file (all granted) and return all file data
 def readJSON(filepath):
     # parse file
-    contain = json.loads()
     with open(filepath) as json_file:
-    data = json.load(json_file)
-    print('Name: ' + data['name'])
-    for expense in data['expensesList']:
-        print('ExpenseName: ' + expense['name'])
+        data = json.load(json_file)
+        print('Name: ' + data['name'])
+        for expense in data['expensesList']:
+            print('ExpenseName: ' + expense['name'])
+        print(len(data['expensesList']))
+    return data
+
+def writeExpense(filename, data, expense):
+    expenseID = len(listExpenses)
+    # convert dict to json
+    newExpense = json.dumps(expense)
+    newExpense.append(expenseID)
+    data['expensesList'].append(newExpense)
+    with open(filename, 'w') as json_file:
+        json.dump(data, json_file)
+    return True
 
 def passwordIsRight (file, password):
     return True
