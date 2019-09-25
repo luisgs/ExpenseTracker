85f58c78f404c9dbc5ddf94374264b9c87b11242
LuisGS <luis.ild@gmail.com>
2019-09-25 11:50:16 +0200

    REFACTORING key/label for each input value

--- expensesinput.py
+++ expensesinput.py
@@ -2,39 +2,56 @@ import sys
 import PySimpleGUI as sg
 import expenseJSONFile
 
-# Very basic window.  Return values as a list
+#
+# Default global variables
+#
 username = "Default"
 filename = "/tmp/deleteme.txt"
+T1_KEY = 'TAB_1'
+T2_KEY = 'TAB_2'
+T3_KEY = 'TAB_3'
 
-categories = [[sg.Radio('Loging', "CAT", default=True, key="cat0"), sg.Radio('Transport', "CAT", key="cat1"), sg.Radio('Entertainemt', "CAT", key="cat2"), sg.Radio('Salary', "CAT", key="cat3")]]
 #
 # First tab layaout
 #
-
+categories = [[sg.Radio('Loging', "CAT", default=True, key=T1_KEY+"_CAT0_"),
+                sg.Radio('Transport', "CAT", key=T1_KEY+"_CAT1_"),
+                sg.Radio('Entertainemt', "CAT", key=T1_KEY+"_CAT2_"),
+                sg.Radio('Salary', "CAT", key=T1_KEY+"_CAT3_")]]
+#
+# CONTINUATION OF First tab layaout
+# ..we use T1_key for labeling our inputs
 tab1_layout =  [
-          [sg.Text('Expense Name', size=(15, 1)), sg.InputText('Expense Name', key='expenseName')],
-          [sg.Text('Quantity', size=(15, 1)), sg.InputText('$?', key='qty')],
-          [sg.Text('Frequency', size=(15, 1)), sg.Radio('Monthly', "FREQ", key="freq0", default=True), sg.Radio('Yearly', "FREQ", key="freq1")],
+          [sg.Text('Expense Name', size=(15, 1)),
+            sg.InputText('Expense Name', key=T1_KEY+'_EXPENSENAME_')],
+          [sg.Text('Quantity', size=(15, 1)), sg.InputText('$?', key=T1_KEY+'_QTY_')],
+          [sg.Text('Frequency', size=(15, 1)),
+            sg.Radio('Monthly', "FREQ", key=T1_KEY+"_FREQ0_", default=True),
+            sg.Radio('Yearly', "FREQ", key=T1_KEY+"_FREQ1_")],
           [sg.Frame("Categories", [[sg.Column(categories)]])],
-          [sg.Text('Date', size=(15, 1)), sg.InputText('../../..', key='date')],
-          [sg.Text('Income/Outcome', size=(15, 1)), sg.Radio('Outcome', "INOUT", default=True, key='in'), sg.Radio('Income', "INOUT", key='out')],
-          [sg.Submit(key='submitTab1'), sg.Cancel(key='Cancel')]
+          [sg.Text('Date', size=(15, 1)), sg.InputText('../../..', key=T1_KEY+'_DATE_')],
+          [sg.Text('Income/Outcome', size=(15, 1)),
+            sg.Radio('Outcome', "INOUT", default=True, key=T1_KEY+'_IN_'),
+            sg.Radio('Income', "INOUT", key=T1_KEY+'_OUT_')],
+          [sg.Submit(key=T1_KEY+'_SUBMIT_'), sg.Cancel(key=T1_KEY+'_CANCEL_')]
          ]
 
 #
 # Second tab layaout
-#
-tab2_layout = [[sg.T('This is inside tab 2')], [sg.In(key='in1')],
-          [sg.Submit(key='submitTab2'), sg.Cancel(key='Cancel')]]
+# WE HAVE TO USE KEY TAB_2
+tab2_layout = [[sg.T('This is inside tab 2')], [sg.In(key=T2_KEY+'_IN_')],
+          [sg.Submit(key=T2_KEY+'_SUBMIT_'), sg.Cancel(key=T2_KEY+'_CANCEL_')]]
 
 
 #
 # Third tab layaout
 #
 tab3_layout = [[sg.T('This is inside tab 3')],
-                      [sg.Submit(key='submitTab3'), sg.Cancel(key='Cancel')]]
+                      [sg.Submit(key=T3_KEY+'_SUBMIT_'), sg.Cancel(key=T3_KEY+'_CANCEL_')]]
 
-layout = [[sg.TabGroup([[sg.Tab('New Expense', tab1_layout), sg.Tab('Expense Report', tab2_layout), sg.Tab('List of Expenses', tab3_layout)]])]]
+layout = [[sg.TabGroup([[sg.Tab('New Expense', tab1_layout),
+            sg.Tab('Expense Report', tab2_layout),
+            sg.Tab('List of Expenses', tab3_layout)]])]]
 
 def main(argv):
     # print(argv)
