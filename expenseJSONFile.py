bb75c542eb07ab67842d485a5f03652c712c4eb2
LuisGS <luis.ild@gmail.com>
2019-09-25 17:22:52 +0200

    Expense data is translatted and checked for formatting and written in our file

--- expenseJSONFile.py
+++ expenseJSONFile.py
@@ -6,6 +6,23 @@ logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 # Library for reading our JSON file
 #
 
+#
+# Global variables
+#
+conversionDict = {
+    "TAB_1_CAT0_Loging": "loging",
+    "TAB_1_CAT1_Transport": "Transport",
+    "TAB_1_CAT2_Entertainment": "Entertainment",
+    "TAB_1_CAT3_Salary": "salary",
+    "TAB_1_DATE_": "date",
+    "TAB_1_EXPENSENAME_": "Expense Name",
+    "TAB_1_FREQ0_Monthly": "Monthly",
+    "TAB_1_FREQ1_Yearly": "Yearly",
+    "TAB_1_IN_": "income",
+    "TAB_1_OUT_": "outcome",
+    "TAB_1_QTY_": "qty",
+}
+
 # readJSON
 # OUT: Open file (all granted) and return all file data
 def readJSON(filepath):
@@ -18,6 +35,17 @@ def readJSON(filepath):
         #logging.debug(len(data['expensesList']))
     return data
 
+#
+# formatExpense
+# formatting a new expense dict into our JSON key values dict
+#
+def formatExpense(newExpense):
+    global conversionDict
+    formattedExpense={}
+    for key,value in newExpense.items():
+        formattedExpense[conversionDict[key]] = value
+    return formattedExpense
+
 # writeExpense
 # IN: we receive an expense dict and filename filepath
 #       we append this new (already formatted) expense with the rest
@@ -28,11 +56,9 @@ def writeExpense(filename, expense):
     # Create new ID for our new expense based on length
     expenseID = len(data['expensesList'])
     # We sort out our expense input and formatted
-    newExpense = expense
+    newExpense = formatExpense(expense)
     # adding it to our NewExpense
     newExpense.update({'expenseID':expenseID})
-    # convert expense dict to json
-    newExpense = json.dumps(expense)
     # Appending our new expense to our LIST of expenses (if any)
     data['expensesList'].append(newExpense)
     try:
